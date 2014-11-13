563b9eefa6557fc540a9aeaaa36007863a2b063e
Anton Berezin <anton.berezin@f-secure.com>
2014-11-13 09:34:16 +0200

    Commented out read the docs theme since it is the one used by default

--- documentation/conf.py
+++ documentation/conf.py
@@ -14,7 +14,7 @@
 
 import sys
 import os
-import sphinx_rtd_theme
+# import sphinx_rtd_theme
 
 
 DOCDIR = os.path.dirname(__file__)
@@ -103,14 +103,14 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "sphinx_rtd_theme"
+# html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
