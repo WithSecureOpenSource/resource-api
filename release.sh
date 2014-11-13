22c067de69d5d583716259a64d15deda923c128e
Anton Berezin <anton.berezin@f-secure.com>
2014-11-13 10:58:16 +0200

    Change docs version on release

--- release.sh
+++ release.sh
@@ -3,3 +3,4 @@
 version=`cat CHANGES  | cut -f 1 -d " "`
 
 sed -i "s/SOURCE_VERSION = .*/SOURCE_VERSION = \"$version\"/g" src/setup_*.py
+sed -i "s/version = .*/version = '$version'/g" documentation/conf.py
