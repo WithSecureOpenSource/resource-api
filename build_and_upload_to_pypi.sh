095cbe88e03ccd9445cb66449908f761f4def320
Anton Berezin <anton.berezin@f-secure.com>
2014-11-19 12:52:28 +0200

    Added build_and_upload_to_pypi.sh

---
+++ build_and_upload_to_pypi.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+BRANCH_NAME=`git symbolic-ref --short HEAD`
+
+if [ "$BRANCH_NAME" == "master" ]; then
+
+    if [ -z "$PIP_PASSWORD" ]; then
+      echo "PIP_PASSWORD env variable is not set. Check Travis CI settings."
+    fi
+
+    cp travis.pypirc.in ~/.pypirc
+    sed -i"" "s/__PASSWORD/$PIP_PASSWORD/g" ~/.pypirc
+
+    cd src
+
+    cp setup_resource_api.py setup.py
+    python setup.py sdist upload
+
+    cp setup_resource_api_http.py setup.py
+    python setup.py sdist upload
+
+    cp setup_resource_api_http_client.py setup.py
+    python setup.py sdist upload
+
+    rm ~/.pypirc
+fi
